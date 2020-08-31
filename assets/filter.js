56eaa55d8bfd300fd63d5abbd83e79de727aff61
Tess Rinearson <tess.rinearson@gmail.com>
2020-08-31 22:34:00 +0200

    working towards sortable tables

---
+++ assets/filter.js
@@ -0,0 +1,27 @@
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+$(document).ready(function () {
+    var $rows = $('#filterable tbody tr');
+    $("#filter").keyup(debounce(function () {
+        var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+        $rows.show().filter(function () {
+            var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+            return !~text.indexOf(val);
+        }).hide();
+    }, 250));
+    $("#filter").prop("disabled", false);
+    $("#filter").focus();
+});
